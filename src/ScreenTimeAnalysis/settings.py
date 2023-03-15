f7e68ad62f35499529970a06b869afca75e7ce8f
Simran <59317835+simrannakarmi@users.noreply.github.com>
2023-03-15 13:10:56 +0545

    Update settings.py

--- src/ScreenTimeAnalysis/settings.py
+++ src/ScreenTimeAnalysis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sloqrw8w(0idowamel^r4(r7jq&ajcc8zyc%)ygr@-wj-@78+8'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
